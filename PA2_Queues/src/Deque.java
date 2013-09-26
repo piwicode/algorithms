48473e99411b737c31c504df08e64f005c99cd83
piwi <pierre.labatut@gmail.com>
2013-09-26 23:21:09 +0300

    queue draft

---
+++ PA2_Queues/src/Deque.java
@@ -0,0 +1,125 @@
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/*
+ * Solution proposal to coursea Algorithms Part 1
+ * Programming Assignment 2: Randomized Queues and Deques
+ */
+
+public class Deque<Item> implements Iterable<Item> {
+
+    private static class Element<Item> {
+        final Item item;
+        Element<Item> prev, next;
+        public Element(Item item, Element<Item> prev, Element<Item> next) {
+            if(item==null) throw new NullPointerException();
+            this.item = item;
+            this.next = next;
+        }
+    }
+    private int size;
+    private Element<Item>  first;
+    private Element<Item>  last;
+
+    /**
+     * construct an empty deque
+     */
+    public Deque() {
+    }
+
+    /**
+     * is the deque empty?
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * return the number of items on the deque
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * insert the item at the front
+     */
+    public void addFirst(Item item) {
+        
+        first = new Element<Item>(item,null, first);        
+        if(first.next!=null) first.next.prev=first;
+        else last=first;
+        size++;
+    }
+
+    /**
+     * insert the item at the end
+     */
+    public void addLast(Item item) {        
+        last = new Element<Item>(item,last,null);
+        if(last.prev!=null) last.prev.next=last;
+        else first=last;
+        size++;
+    }
+
+    /**
+     * delete and return the item at the front
+     */
+    public Item removeFirst() {
+        if(isEmpty())throw new NoSuchElementException();
+        size--;
+        Element<Item> e = first;
+        first = e.next;
+        if(first!=null) first.prev=null;
+        else last=null;
+        return e.item;
+    }
+
+    /**
+     * delete and return the item at the end
+     */
+    public Item removeLast() {
+        if(isEmpty())throw new NoSuchElementException();
+        size--;
+        Element<Item> e = last;
+        last = e.prev;
+        if(last!=null) last.next=null;
+        else first=null;
+        return e.item;
+    }
+
+    /**
+     * return an iterator over items in order from front to end
+     */
+    public Iterator<Item> iterator() {
+        return new Iterator<Item>() {
+            Element<Item> cur = first;
+            @Override
+            public boolean hasNext() {
+                return cur!=null;
+            }
+
+            @Override
+            public Item next() {
+                if(!hasNext())throw new NoSuchElementException();
+                Item i=cur.item;
+                cur=cur.next;
+                return i;
+            }
+
+            @Override
+            public void remove() {
+                if(!hasNext())throw new NoSuchElementException();
+                
+                if(cur==first) removeFirst();
+                else if(cur==last) removeLast();
+                else{
+                    size--;
+                    cur.prev.next=cur.next;
+                    cur.next.prev=cur.prev;
+                }
+            }
+        };
+    }
+}
---
+++ PA2_Queues/test/DequeTest.java
@@ -0,0 +1,112 @@
+/*
+ * Solution proposal to coursea Algorithms Part 1
+ * Programming Assignment 2: Randomized Queues and Deques
+ */
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Pierre
+ */
+public class DequeTest {
+
+    Deque<Integer> deq = new Deque<>();
+
+    public DequeTest() {
+    }
+
+    @Test
+    public void testIsEmpty() {
+        assertTrue(deq.isEmpty());
+        assertEquals(0, deq.size());
+
+        deq.addFirst(111);
+        assertFalse(deq.isEmpty());
+        assertEquals(1, deq.size());
+
+        assertEquals(111, deq.removeFirst().intValue());
+        assertTrue(deq.isEmpty());
+        assertEquals(0, deq.size());
+
+        deq.addFirst(222);
+        assertFalse(deq.isEmpty());
+        assertEquals(1, deq.size());
+
+        assertEquals(222, deq.removeLast().intValue());
+        assertTrue(deq.isEmpty());
+        assertEquals(0, deq.size());
+
+        deq.addLast(333);
+        assertFalse(deq.isEmpty());
+        assertEquals(1, deq.size());
+
+        assertEquals(333, deq.removeFirst().intValue());
+        assertTrue(deq.isEmpty());
+        assertEquals(0, deq.size());
+
+        deq.addLast(444);
+        assertFalse(deq.isEmpty());
+        assertEquals(1, deq.size());
+
+        assertEquals(444, deq.removeLast().intValue());
+        assertTrue(deq.isEmpty());
+        assertEquals(0, deq.size());
+
+        deq.addFirst(2);
+        deq.addLast(3);
+        deq.addFirst(1);
+        deq.addLast(4);
+        deq.addLast(5);
+        deq.addFirst(0);
+        
+        assertEquals(6, deq.size());
+        Iterator<Integer> iterator = deq.iterator();
+        assertTrue(iterator.hasNext());
+        assertEquals(0, iterator.next().intValue());
+        assertTrue(iterator.hasNext());
+        assertEquals(1, iterator.next().intValue());
+        assertTrue(iterator.hasNext());
+        assertEquals(2, iterator.next().intValue());
+        assertTrue(iterator.hasNext());
+        assertEquals(3, iterator.next().intValue());
+        assertTrue(iterator.hasNext());
+        assertEquals(4, iterator.next().intValue());
+        assertTrue(iterator.hasNext());
+        assertEquals(5, iterator.next().intValue());
+        assertFalse(iterator.hasNext());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testSize() {
+        deq.addFirst(null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testAddFirst() {
+        deq.addLast(null);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testAddLast() {
+        deq.removeFirst();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testRemoveFirst() {
+        deq.removeLast();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testRemoveLast() {
+        deq.iterator().remove();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testRemoveLast2() {
+        deq.iterator().next();
+    }
+
+}
