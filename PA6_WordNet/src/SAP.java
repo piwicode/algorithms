ca10ade7bb9e6374c7cae50c3fa1dcf28e0a04f2
piwi <pierre.labatut@gmail.com>
2013-12-08 12:27:12 +0300

    Double DFS implementation

--- PA6_WordNet/src/SAP.java
+++ PA6_WordNet/src/SAP.java
@@ -1,99 +1,60 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /*
  * Solution proposal to coursea Algorithms Part 2
  * Programming Assignment 1: WordNet
  */
-
 public class SAP {
 
     private final Digraph dg;
-    private final int halfSize;
-    private int bfsId = -1;
-    private BreadthFirstDirectedPaths bfs;
+    private int lastd, lasta;
+    int queue[], qh, qt;
+    private final int d[];
+    private final byte c[];
 
     // constructor takes a digraph (not necessarily a DAG)
     // ~V+3*E
     public SAP(Digraph in) {
-        halfSize = in.V();
-        this.dg = new Digraph(halfSize * 2);
-        //Fold the graph on itself       
-        for (int i = 0; i < halfSize; i++) {
-            for (int j : in.adj(i)) {
-                dg.addEdge(i, j);
-                dg.addEdge(j + halfSize, i + halfSize);
-            }
-            //Connect the graph and the reversed grapg
-            dg.addEdge(i, i + halfSize);
-        }
+        this.dg = new Digraph(in);
+        d = new int[in.V()];
+        queue = new int[in.V() * 2];
+        c = new byte[in.V()];
+        Arrays.fill(d, Integer.MAX_VALUE);
+        Arrays.fill(c, (byte) 0);
     }
 
     // length of shortest ancestral path between v and w; -1 if no such path
     // ~V+E
     public int length(int v, int w) {
-        checkInputBound(v);
-        checkInputBound(w);
+        return length(Collections.singletonList(v), Collections.singletonList(w));
 
-        // Check the cache
-        if (w == bfsId) {
-            w = v;
-            v = bfsId;
-        }
-        if (v != bfsId) {
-            bfsId = v;
-            bfs = new BreadthFirstDirectedPaths(dg, v);
-        }
-        final int distTo = bfs.distTo(w + halfSize);
-        /*
-         Iterable<Integer> pathTo = bfs.pathTo(w + halfSize);
-         for (int i : pathTo) {
-         if (i != v) System.out.print("->");
-         System.out.print(i);
-         }
-         System.out.println();
-         */
-        //Removes 1 to the length to ignore the edge connecting the graph and its mirror image
-        return distTo == Integer.MAX_VALUE ? -1 : distTo - 1;
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
     // ~V+E
     public int ancestor(int v, int w) {
-        checkInputBound(v);
-        checkInputBound(w);
-        //Check the cache
-        if (w == bfsId) {
-            w = v;
-            v = bfsId;
-        }
-        if (v != bfsId) {
-            bfsId = v;
-            bfs = new BreadthFirstDirectedPaths(dg, v);
-        }
-        return ancestorOfPath(bfs.pathTo(w + halfSize));
+        return ancestor(Collections.singletonList(v), Collections.singletonList(w));
     }
 
     /**
-     * length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
+     * length of shortest ancestral path between any vertex in v and any vertex
+     * in w; -1 if no such path
+     *
      * @throws IllegalAccessException is vs or ws elements are out of [0,dg.V()]
-     * @throws NullPointerException 
-     */ 
-    public int length(final Iterable<Integer> vs, final Iterable<Integer> ws) {
-        checkBound(vs);
-        checkBound(ws);
-        final BreadthFirstDirectedPaths mbfs = new BreadthFirstDirectedPaths(dg, vs);        
-        final int to = closestFrom(ws, mbfs);
-        if (to == -1) return -1;
-        final int distTo = mbfs.distTo(to+halfSize);
-        return distTo == Integer.MAX_VALUE ? -1 : distTo - 1;
+     * @throws NullPointerException
+     */
+    public int length(Iterable<Integer> vs, Iterable<Integer> ws) {
+        doubleDfs(vs, ws);
+        return lastd;
     }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no such path
     public int ancestor(Iterable<Integer> vs, Iterable<Integer> ws) {
-        checkBound(vs);
-        checkBound(ws);
-        final BreadthFirstDirectedPaths mbfs = new BreadthFirstDirectedPaths(dg, vs);
-        int to = closestFrom(ws, mbfs);
-        if (to == -1) return -1;
-        return ancestorOfPath(mbfs.pathTo(to + halfSize));
+        doubleDfs(vs, ws);
+        return lasta;
     }
 
     // for unit testing of this class (such as the one below)
@@ -110,34 +71,64 @@ public class SAP {
         }
     }
 
-    private void checkInputBound(int v) throws IndexOutOfBoundsException  {
-        if (v >= halfSize || v < 0) throw new IndexOutOfBoundsException ();
-    }
-
-    private void checkBound(Iterable<Integer> vs) throws IndexOutOfBoundsException  {
+    private void checkBound(Iterable<Integer> vs) throws IndexOutOfBoundsException {
         for (int v : vs) {
-            checkInputBound(v);
+            if (v >= dg.V() || v < 0) throw new IndexOutOfBoundsException();
         }
     }
 
-    private int ancestorOfPath(Iterable<Integer> path) throws RuntimeException {
-        if (path == null) return -1;
-        for (int node : path) {
-            if (node >= halfSize)
-                return node - halfSize;
-        }
-        throw new RuntimeException("should never happend");
-    }
+    private void doubleDfs(Iterable<Integer> vs, Iterable<Integer> ws) {
+        checkBound(vs);
+        checkBound(ws);
 
-    private int closestFrom(Iterable<Integer> ws, final BreadthFirstDirectedPaths mbfs) {
-        int to = -1, min = Integer.MAX_VALUE;
+        for (int i = 0; i < qt; i++) {
+            d[queue[i]] = Integer.MAX_VALUE;
+            c[queue[i]] = 0;
+        }
+        assert cleanState();
+        qh = 0;
+        qt = 0;
+        for (int v : vs) {
+            queue[qt++] = v;
+            d[v] = 0;
+            c[v] = 1;
+        }
         for (int w : ws) {
-            final int d = mbfs.distTo(w + halfSize);
-            if (d < min) {
-                to = w;
-                min = d;
+            queue[qt++] = w;
+            d[w] = 0;
+            if (c[w] == 1) {
+                lasta = w;
+                lastd = 0;
+                return;
+            }
+            c[w] = 2;
+        }
+        while (qh < qt) {
+            int n = queue[qh++];
+            for (int to : dg.adj(n)) {
+                if (c[to] == 0) {
+                    queue[qt++] = to;
+                    c[to] = c[n];
+                    d[to] = d[n] + 1;
+                } else if (c[to] != c[n]) {
+                    lasta = to;
+                    lastd = d[to] + d[n] + 1;
+                    return;
+                }
             }
         }
-        return to;
+        lasta = -1;
+        lastd = -1;
     }
+
+    private boolean cleanState() {
+        for (int v : d) {
+            if (v != Integer.MAX_VALUE) return false;
+        }
+        for (byte v : c) {
+            if (v != (byte) 0) return false;
+        }
+        return true;
+    }
+
 }
